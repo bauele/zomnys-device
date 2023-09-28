1191da37939219e651eab72fb080c65674109ebe
bauele <bauele@users.noreply.github.com>
2023-09-28 10:08:11 -0400

    Move react app into the client folder of root directory

---
+++ client/src/util/sleepRecord.ts
@@ -0,0 +1,18 @@
+export default class SleepRecord {
+    id: string;
+    timestampStart: Date;
+    timestampEnd: Date;
+    awakeningCount: number;
+
+    constructor(
+        id: string,
+        timestampStart: Date,
+        timestampEnd: Date,
+        awakeningCount: number,
+    ) {
+        this.id = id;
+        this.timestampStart = timestampStart;
+        this.timestampEnd = timestampEnd;
+        this.awakeningCount = awakeningCount;
+    }
+}
